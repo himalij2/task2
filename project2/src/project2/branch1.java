95182e9314f6a0aaf62ccbfa21a8491d95e711f6
sdac5 <sdac5@sdac5-PC>
2023-06-14 15:27:15 +0530

    branch 1

--- project2/src/project2/branch1.java
+++ project2/src/project2/branch1.java
@@ -4,7 +4,7 @@ public class branch1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("branch 1 created");
+System.out.println("branch 1 created ");
 	}
 
 }
